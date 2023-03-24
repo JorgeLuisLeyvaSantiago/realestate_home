02cc10ca9f823df51a0552e5d84e2c40f34f1312
Jorge Leyva <jleyvas@uni.pe>
2023-03-24 01:15:15 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Crear una variable de cadena
+var text_information = "Bienvenidos! Welcome! Ayniykuy! En primer lugar, queremos destacar la importancia de elegir una buena ubicación. Es fundamental buscar un lugar cerca de centro de estudios, con conexión de transporte público y con servicios básicos como tiendas, mercados y restaurantes cercanos. En conclusión, elegir una buena ubicación y la opción de alquiler adecuada puede hacer la diferencia en la calidad de vida durante la época de estudios o para parejas sin hijos que buscan un lugar cómodo y asequible para vivir.<br><br>No pierdas esta oportunidad de vivir en un lugar cómodo, ¡contáctanos hoy mismo para programar una visita y conocer tu próxima habitación! ";
+
+
+// Crear una variable numérica
+var edad = 30;
